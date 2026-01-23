15bd3811b60cb643a0608985cdb6011706831bdb
Liu Zhiyuan <e1481827@u.nus.edu>
2026-01-23 10:41:19 +0800

    Complete Level-1

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Zack {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -5,13 +7,29 @@ public class Zack {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
+        System.out.println("Hello from\n" + logo);
+        */
+
         String line = "____________________________________________________________";
-        System.out.println(line);
-        System.out.println("Hello! I'm Zack");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        System.out.println(" " + line);
+        System.out.println(" Hello! I'm Zack");
+        System.out.println(" What can I do for you?");
+        System.out.println(" " + line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            System.out.println(" " + line);
+
+            if (input.equals("bye")) {
+                System.out.println("  Bye. Hope to see you again soon!");
+                System.out.println(" " + line);
+                break;
+            }
+            System.out.println("  " + input);
+            System.out.println(" " + line);
+        }
     }
 }
