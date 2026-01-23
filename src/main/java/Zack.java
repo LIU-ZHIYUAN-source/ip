d6c7c017cddd979f3164f5d1bfaf7df891d09f34
Liu Zhiyuan <e1481827@u.nus.edu>
2026-01-23 10:17:52 +0800

    Revise Level-0 output with underscore lines

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -6,8 +6,12 @@ public class Zack {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
+        String line = "____________________________________________________________";
+        System.out.println(line);
         System.out.println("Hello! I'm Zack");
         System.out.println("What can I do for you?");
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
