32c07aaf80389109f24724038834506eb9c0c529
Liu Zhiyuan <e1481827@u.nus.edu>
2026-01-23 11:12:07 +0800

    Complete Level-2

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -11,25 +11,40 @@ public class Zack {
         */
 
         String line = "____________________________________________________________";
+        String[] items = new String[100];
+        int count = 0;
 
-        System.out.println(" " + line);
+        System.out.println("  " + line);
         System.out.println(" Hello! I'm Zack");
         System.out.println(" What can I do for you?");
-        System.out.println(" " + line);
+        System.out.println("  " + line);
 
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             String input = scanner.nextLine();
 
-            System.out.println(" " + line);
+            System.out.println("  " + line);
 
             if (input.equals("bye")) {
-                System.out.println("  Bye. Hope to see you again soon!");
-                System.out.println(" " + line);
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("  " + line);
                 break;
             }
-            System.out.println("  " + input);
-            System.out.println(" " + line);
+
+            if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println("    " + (i + 1) + ". " + items[i]);
+                }
+                System.out.println("  " + line);
+                continue;
+            }
+
+            items[count] = input;
+            count++;
+
+            System.out.println("    added: " + input);
+            System.out.println("  " + line);
         }
     }
 }
